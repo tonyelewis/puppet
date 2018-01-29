70afaccdb57cbe466862785edc5a43986dd04863
Tony Lewis <tonyelewis@users.noreply.github.com>
2018-01-29 08:48:45 +0000

    Tweak: fix minor typos in comments

--- modules/cpp_devel/manifests/init.pp
+++ modules/cpp_devel/manifests/init.pp
@@ -15,10 +15,10 @@
 # Here you should define a list of variables that this module would require.
 #
 # [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
+#   Explanation of how this variable affects the function of this class and if
 #   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
 #   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
+#   global variables should be avoided in favour of class parameters as
 #   of Puppet 2.6.)
 
 class cpp_devel {
