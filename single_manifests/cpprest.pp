42f9f0ca5197e023a6bc3802e5326c04e0e8c15c
Tony Lewis <tonyelewis@users.noreply.github.com>
2018-09-14 07:46:43 +0100

    Add a gcc-debug-checked version

--- single_manifests/cpprest.pp
+++ single_manifests/cpprest.pp
@@ -34,16 +34,16 @@ file { "Source dir ${user_src_dir}":
   user     => $user,
 }
 
+$tag_compiler_and_cmake_flags_sets = [
+  [ 'clang',             'clang', ' -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_CXX_FLAGS=" -stdlib=libc++ ${CMAKE_CXX_FLAGS} " ' ],
+  [ 'gcc',               'gcc',   ''                                                                                                                                  ],
+  [ 'gcc-debug-checked', 'gcc',   ' -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS=" -D_GLIBCXX_DEBUG=1 ${CMAKE_CXX_FLAGS} " '                                            ],
+]
 
-$compiler_and_cmake_flags_pairs = {
-  'clang' => ' -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_CXX_FLAGS="-stdlib=libc++" ',
-  'gcc'   => '',
-}
-
-
-$compiler_and_cmake_flags_pairs.each | String $compiler, String $cmake_flags | {
-  $build_dir   = "${release_dir}/${compiler}_build"
-  $install_dir = "${user_src_dir}/cpprestsdk-${compiler}"
+$tag_compiler_and_cmake_flags_sets.each | Array $tag_compiler_and_cmake_flags_set | {
+  [ $tag, $compiler, $cmake_flags] = $tag_compiler_and_cmake_flags_set
+  $build_dir   = "${release_dir}/${tag}_build"
+  $install_dir = "${user_src_dir}/cpprestsdk-${tag}"
 
   file { $build_dir:
     ensure  => 'directory',
@@ -56,21 +56,21 @@ $compiler_and_cmake_flags_pairs.each | String $compiler, String $cmake_flags | {
     group  => $user,
     owner  => $user,
   }
-  ->exec { "cmake cpprest ${compiler}":
+  ->exec { "cmake cpprest ${tag}":
     command => "cmake -GNinja -B${build_dir} -H${release_dir} -DBOOST_ROOT=/opt/boost_1_68_0_${compiler}_c++14_build -DWERROR:BOOL=OFF -DCMAKE_INSTALL_RPATH_USE_LINK_PATH:BOOL=ON -DCMAKE_INSTALL_PREFIX=${install_dir} ${cmake_flags}",
     creates => "${build_dir}/build.ninja",
     group   => $user,
     require => Package[ 'libssl-dev' ],
     user    => $user,
   }
-  ->exec { "build cpprest ${compiler}":
+  ->exec { "build cpprest ${tag}":
     command => "ninja -j 2 -C ${build_dir}",
     creates => "${build_dir}/Binaries/libcpprest.so",
     group   => $user,
     timeout => 3600, # Defaults to 300s
     user    => $user,
   }
-  ->exec { "install cpprest ${compiler}":
+  ->exec { "install cpprest ${tag}":
     command => "ninja -j 2 -C ${build_dir} install",
     creates => "${install_dir}/lib/libcpprest.so",
     group   => $user,
