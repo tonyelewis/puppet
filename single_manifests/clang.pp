22fdd42db0fca0aaf85f6f4115f698649a6d2fcb
Tony Lewis <tonyelewis@users.noreply.github.com>
2019-03-17 20:09:23 +0000

    Tweak clang building commands

--- single_manifests/clang.pp
+++ single_manifests/clang.pp
@@ -9,12 +9,12 @@
 # svn co    http://llvm.org/svn/llvm-project/libcxx/trunk            ~/source/llvm-master/projects/libcxx
 
 
+# mkdir ~/source/llvm{,-build,-master}
 # git clone https://git.llvm.org/git/llvm.git/                       ~/source/llvm-master
 # git clone https://git.llvm.org/git/clang.git/                      ~/source/llvm-master/tools/clang
 # git clone https://git.llvm.org/git/clang-tools-extra.git/          ~/source/llvm-master/tools/clang/tools/extra
 # git clone https://git.llvm.org/git/compiler-rt.git/                ~/source/llvm-master/projects/compiler-rt
 # git clone https://git.llvm.org/git/libcxx.git/                     ~/source/llvm-master/projects/libcxx
 # git clone https://git.llvm.org/git/libcxxabi.git/                  ~/source/llvm-master/projects/libcxxabi
-# mkdir ~/source/llvm{,-build}
 # cmake -B`ls -1d ~/source/llvm-build` -H`ls -1d ~/source/llvm-master` -GNinja -DCMAKE_INSTALL_PREFIX=`ls -1d ~/source/llvm` -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=OFF
 # ninja -C ~/source/llvm-build -k 0 -j 4
