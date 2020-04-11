ba27fb7dd681d44c4fc8432ff580d442d693b1ac
Tony Lewis <tonyelewis@users.noreply.github.com>
2020-04-11 10:14:48 +0100

    Update to new cpprestsdk locations (and tidy up)

--- toolchain-files/clang_dbgchk.cmake
+++ toolchain-files/clang_dbgchk.cmake
@@ -1,9 +1,14 @@
 set( ENV{Boost_DIR}              /opt/boost_1_72_0_dbgchk_c++17                                                                                               )
 set( CMAKE_BUILD_TYPE            DEBUG                                                                                                                        )
-set( CMAKE_PREFIX_PATH           "/opt/Qt/this-version/gcc_64;$ENV{HOME}/source/cpprestsdk-clang;$ENV{HOME}/source/QtAV-clang;$ENV{HOME}/source/vlc-qt-clang" )
+set( CMAKE_PREFIX_PATH           "/opt/Qt/this-version/gcc_64;$ENV{HOME}/source/cpprestsdk-clang_dbgchk;$ENV{HOME}/source/QtAV-clang;$ENV{HOME}/source/vlc-qt-clang" )
 set( CMAKE_C_COMPILER            "$ENV{HOME}/source/llvm/bin/clang"                                                                                           )
 set( CMAKE_CXX_COMPILER          "$ENV{HOME}/source/llvm/bin/clang++"                                                                                         )
 set( CMAKE_CXX_FLAGS_INIT        " -stdlib=libc++ -D_LIBCPP_DEBUG=0 -DBOOST_ASIO_HAS_STD_STRING_VIEW ${CMAKE_CXX_FLAGS} "                                     )
 set( CMAKE_EXE_LINKER_FLAGS_INIT " -Wl,-rpath=$ENV{HOME}/source/llvm/lib "                                                                                    )
+
+
+
+
+
 
 include( "${CMAKE_CURRENT_LIST_DIR}/misc.cmake" )
--- toolchain-files/clang_rwdi.cmake
+++ toolchain-files/clang_rwdi.cmake
@@ -1,6 +1,6 @@
 set( ENV{Boost_DIR}              /opt/boost_1_72_0_normal_c++17                                                                                               )
 set( CMAKE_BUILD_TYPE            RELWITHDEBINFO                                                                                                               )
-set( CMAKE_PREFIX_PATH           "/opt/Qt/this-version/gcc_64;$ENV{HOME}/source/cpprestsdk-clang;$ENV{HOME}/source/QtAV-clang;$ENV{HOME}/source/vlc-qt-clang" )
+set( CMAKE_PREFIX_PATH           "/opt/Qt/this-version/gcc_64;$ENV{HOME}/source/cpprestsdk-clang_rwdi;$ENV{HOME}/source/QtAV-clang;$ENV{HOME}/source/vlc-qt-clang" )
 set( CMAKE_C_COMPILER            "/usr/bin/clang"                                                                                                             )
 set( CMAKE_CXX_COMPILER          "/usr/bin/clang++"                                                                                                           )
 set( CMAKE_CXX_FLAGS_INIT        " -stdlib=libc++ -DBOOST_ASIO_HAS_STD_STRING_VIEW ${CMAKE_CXX_FLAGS} "                                                       )
