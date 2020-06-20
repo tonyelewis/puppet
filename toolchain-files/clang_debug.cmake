0bad5605803f8f6b6bbf92826bb8aea9f88fd97c
Tony Lewis <tonyelewis@users.noreply.github.com>
2020-06-20 12:11:32 +0100

    Update the Boost the toolchain files use to 1.73.0

--- toolchain-files/clang_debug.cmake
+++ toolchain-files/clang_debug.cmake
@@ -1,4 +1,4 @@
-set( ENV{Boost_DIR}              /opt/boost_1_72_0_normal_c++17                                                                                               )
+set( ENV{Boost_DIR}              /opt/boost_1_73_0_normal_c++17                                                                                               )
 set( CMAKE_BUILD_TYPE            DEBUG                                                                                                                        )
 set( CMAKE_PREFIX_PATH           "/opt/Qt/this-version/gcc_64;$ENV{HOME}/source/cpprestsdk-clang_debug;$ENV{HOME}/source/QtAV-clang;$ENV{HOME}/source/vlc-qt-clang" )
 set( CMAKE_C_COMPILER            "/usr/bin/clang"                                                                                                             )
