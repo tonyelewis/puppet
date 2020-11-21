04b2de4af61edc6fdbd78c89058b5aeb9f5084f1
Tony Lewis <tonyelewis@users.noreply.github.com>
2020-11-21 15:30:25 +0000

    Add some project conan/cmake files

---
+++ toolchain-files/project/default.cmake
@@ -0,0 +1,1 @@
+set( CMAKE_CXX_FLAGS_INIT " ${CMAKE_CXX_FLAGS_INIT} -W -Wall -Werror -Wextra -Wcast-qual -Wconversion -Wformat=2 -Wnon-virtual-dtor -Wshadow -Wsign-compare -Wsign-conversion -pedantic -ftabstop=2 " )
