cb786b9169e7160df3eeac355628000256a313b3
Tony Lewis <tonyelewis@users.noreply.github.com>
2021-05-03 09:31:35 +0100

    Point to correct ffmpeg for static build

--- toolchain-files/project/edit-suite.cmake
+++ toolchain-files/project/edit-suite.cmake
@@ -3,3 +3,7 @@ if ( NOT EDIT_SUITE_TOOLCHAIN_FILE_HAS_APPENDED_TO_PREFIX_PATH )
 	list( APPEND CMAKE_PREFIX_PATH "$ENV{HOME}/source/vlc-qt-gcc" )
 	option( EDIT_SUITE_TOOLCHAIN_FILE_HAS_APPENDED_TO_PREFIX_PATH "Whether this edit-suite toolchain file has already appended to the CMAKE_PREFIX_PATH" ON )
 endif()
+
+if ( DEFINED BUILD_SHARED_LIBS AND NOT BUILD_SHARED_LIBS )
+	list( APPEND CMAKE_PREFIX_PATH "/opt/ffmpeg-4.4" )
+endif()
