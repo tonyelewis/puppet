edf4309e2f72220bd023c78a6a8ef218649988bb
Tony Lewis <tonyelewis@users.noreply.github.com>
2021-05-06 07:01:52 +0100

    Improve handling of default compiler flags

--- toolchain-files/project/default.cmake
+++ toolchain-files/project/default.cmake
@@ -1,23 +1,36 @@
 set( CMAKE_CXX_FLAGS_PRJCT_INIT_LIST
+	-Wcast-qual
+	-Wconversion
+	-Wformat=2
+	-Wimplicit-fallthrough
+	-Wno-deprecated-declarations
+	-Wnon-virtual-dtor
+	-Wnull-dereference
+	-Wshadow
+	-Wsign-compare
+	-Wsign-conversion
+)
+
+if ( CMAKE_CXX_COMPILER MATCHES "clang\\+\\+$"  )
+	list( APPEND CMAKE_CXX_FLAGS_PRJCT_INIT_LIST
+		-Wmove
+	)
+elseif ( CMAKE_CXX_COMPILER MATCHES "g\\+\\+$"  )
+	list( APPEND CMAKE_CXX_FLAGS_PRJCT_INIT_LIST
+		-Wduplicated-branches
+		-Wduplicated-cond
+		-Wnull-dereference
+		-Wrestrict
+	)
+endif()
+
+list( SORT CMAKE_CXX_FLAGS_PRJCT_INIT_LIST )
+list( PREPEND CMAKE_CXX_FLAGS_PRJCT_INIT_LIST
+	-ftabstop=2
+	-pedantic
 	-W
 	-Wall
 	-Werror
 	-Wextra
-	-Wcast-qual
-	-Wconversion
-	-Wformat=2
-	-Wnon-virtual-dtor
-	-Wshadow
-	-Wimplicit-fallthrough
-	-Wsign-compare
-	-Wsign-conversion
-	-pedantic
-	-ftabstop=2
-	-Wno-deprecated-declarations
 )
 string( REPLACE ";" " " CMAKE_CXX_FLAGS_PRJCT_INIT "${CMAKE_CXX_FLAGS_PRJCT_INIT_LIST}" )
-# $<$<AND:$<CXX_COMPILER_ID:GNU>,$<VERSION_GREATER:$<CXX_COMPILER_VERSION>,6.0.0>>:-Wduplicated-cond>
-# $<$<AND:$<CXX_COMPILER_ID:GNU>,$<VERSION_GREATER:$<CXX_COMPILER_VERSION>,6.0.0>>:-Wnull-dereference>
-# $<$<AND:$<CXX_COMPILER_ID:GNU>,$<VERSION_GREATER:$<CXX_COMPILER_VERSION>,7.0.0>>:-Wduplicated-branches>
-# $<$<AND:$<CXX_COMPILER_ID:GNU>,$<VERSION_GREATER:$<CXX_COMPILER_VERSION>,7.0.0>>:-Wrestrict>
-# $<$<AND:$<CXX_COMPILER_ID:GNU>,$<VERSION_GREATER:$<CXX_COMPILER_VERSION>,8.0.0>,$<VERSION_LESS:$<CXX_COMPILER_VERSION>,9.0.0>>:-Wno-maybe-uninitialized>
