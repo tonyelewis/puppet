1312e7faa0780359b3add0582ccd6c4eff7413ba
Tony Lewis <tonyelewis@users.noreply.github.com>
2021-06-04 15:58:07 +0100

    Set up a clang_memsan ignorelist

--- toolchain-files/clang_memsan.cmake
+++ toolchain-files/clang_memsan.cmake
@@ -1,9 +1,9 @@
 set( CMAKE_BUILD_TYPE            Debug                                CACHE STRING   "CMake build type"                                                                                                      )
 set( CMAKE_C_COMPILER            "$ENV{HOME}/source/llvm/bin/clang"   CACHE FILEPATH "The C compiler"                                                                                                        )
 set( CMAKE_CXX_COMPILER          "$ENV{HOME}/source/llvm/bin/clang++" CACHE FILEPATH "The C++ compiler"                                                                                                      )
-set( CMAKE_CXX_FLAGS_TLCHN_INIT  " -fsanitize=memory -fsanitize-memory-track-origins -fPIE -fPIC -fno-omit-frame-pointer -nostdinc++ -I$ENV{HOME}/source/msan-libcxx/include/c++/v1 "                        )
+set( CMAKE_CXX_FLAGS_TLCHN_INIT  " -fsanitize=memory -fsanitize-memory-track-origins -fPIE -fPIC -fno-omit-frame-pointer -nostdinc++ -I$ENV{HOME}/source/msan-libcxx/include/c++/v1 -fsanitize-blacklist=/home/lewis/puppet/toolchain-files/sanitizer-ignorelist " )
 set( CMAKE_EXE_LINKER_FLAGS_INIT " -fsanitize=memory -fpie -stdlib=libc++ -Wno-unused-command-line-argument -L$ENV{HOME}/source/msan-libcxx/lib -Wl,-rpath,$ENV{HOME}/source/msan-libcxx/lib -lc++abi -lc++" )
-
+# TODO: Soon update locally-built clang and then use fsanitize-ignorelist over -fsanitize-blacklist
 
 # VSCode CMake extension likes CMAKE_CXX_COMPILER/CMAKE_C_COMPILER in the cache
 # (though CMake is trying to do this a diffferent way : https://gitlab.kitware.com/cmake/cmake/-/issues/20225)
