c353963d2aae40f0a35e8a6b030a1e4e1e169625
Tony Lewis <tonyelewis@users.noreply.github.com>
2023-05-12 08:05:17 +0100

    More updates to reflect new compiler setup

--- toolchain-files/clang_dbgchk.cmake
+++ toolchain-files/clang_dbgchk.cmake
@@ -2,7 +2,7 @@ set( CMAKE_BUILD_TYPE            Debug                                CACHE STRI
 set( CMAKE_C_COMPILER            "$ENV{HOME}/source/llvm/bin/clang"   CACHE FILEPATH "The C compiler"   )
 set( CMAKE_CXX_COMPILER          "$ENV{HOME}/source/llvm/bin/clang++" CACHE FILEPATH "The C++ compiler" )
 set( CMAKE_CXX_FLAGS_TLCHN_INIT  " -stdlib=libc++ -D_LIBCPP_DEBUG=0 -DBOOST_NO_CXX98_FUNCTION_BASE "    )
-set( CMAKE_EXE_LINKER_FLAGS_INIT " -Wl,-rpath=$ENV{HOME}/source/llvm/lib "                              )
+set( CMAKE_EXE_LINKER_FLAGS_INIT " -Wl,-rpath=$ENV{HOME}/source/llvm/lib/x86_64-unknown-linux-gnu "     )
 
 
 # VSCode CMake extension likes CMAKE_CXX_COMPILER/CMAKE_C_COMPILER in the cache
