17d99a3e234bf828a0859a7318b50e42f52155b0
Tony Lewis <tonyelewis@users.noreply.github.com>
2024-07-06 18:52:41 +0100

    Notes on installing cargo-lambda (after a fashion)

---
+++ single_manifests/cargo-lambda.pp
@@ -0,0 +1,9 @@
+
+# # As yet, no effort has been made to actually puppetize these steps
+# python3 -m venv --prompt venv-cargo-lambda ~/source/venv-cargo-lambda
+# source ~/source/venv-cargo-lambda/bin/activate
+# pip install wheel
+# pip install --upgrade pip
+# pip install cargo-lambda
+# deactivate
+# ln -s ~/source/venv-cargo-lambda/bin/cargo-lambda ~/bin/cargo-lambda
